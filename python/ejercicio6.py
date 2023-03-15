f93ea03d5a5c1bb6f02a08e844d849185926d259
MauCardenas <mc7510466@gmail.com>
2023-03-15 12:13:32 -0600

    figuras

---
+++ python/ejercicio6.py
@@ -0,0 +1,26 @@
+lado = input ("ingresa los lados")
+lado = int (lado)
+
+if(lado ==3):
+    print("es un triangulo")
+else:
+    if(lado ==4):
+        print("es un cuadrado")
+    else:
+        if(lado ==5):
+            print("es un pentagono")
+        else:
+            if(lado ==6):
+                print("es un hexagono")
+            else:
+                if(lado ==8):
+                    print("es un octagono")
+                else:
+                    if(lado ==10):
+                        print("es un decagono")
+                    else:
+                        if(lado ==12):
+                            print("es un dodecagono")
+                        else:
+                            print("dato invalido")
+            
